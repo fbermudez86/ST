e5f9cdddf124991f0f0871992365a5b4db1c23db
Jose Fabio Bermudez <fbermudez86@gmail.com>
2013-08-04 22:53:43 -0600

    Updated code
    Functionality completed

--- src/com/st/src/PatternFinder.groovy
+++ src/com/st/src/PatternFinder.groovy
@@ -1,25 +1,109 @@
 package com.st.src
+import java.io.IOException;
 
 class PatternFinder
 {
+	def directory;
+	def filePattern;
+	def baseDirectory;
+	def outputFileName;
+	def pattern;
+	def newString;
+	def resultTextBuilder = ''<<'';
 
-	void findInPath(baseDirectory, pattern, newString)
+	PatternFinder(String[] params)
 	{
-		def directory = new File(baseDirectory);
-		def filePattern = ~/${pattern}/
-		//def outputFile = args[3];
+		resultTextBuilder <<= 'App started!!\n';
+		this.baseDirectory = params[0];
+		this.pattern = params[1];
+		this.newString = params[2];
+		this.outputFileName = params[3];
+		directory = new File(baseDirectory);
+		filePattern = ~/${pattern}/;
+	}
 
-
-		//Si no es un directorio
+	void findInPath()
+	{
 		if(!directory.isDirectory())
 		{
 			println "The provided directory name ${baseDirectory} is NOT a directory."
 			System.exit(-2)
 		}
-		directory.eachFileRecurse(findFilenameClosure);
+		//Call the closure for each file
+		directory.eachFileRecurse(findPatternClosure);
+
+		resultTextBuilder <<= String.format("Creating output file at: %s \n", this.outputFileName);
+
+		//Write the log file
+		def outputFile = new File(this.outputFileName);
+		outputFile.write(resultTextBuilder.toString());
+		println resultTextBuilder;
 	}
-	def findFilenameClosure =
+
+	def findPatternClosure =
 	{
-		println "\t${it.name} (size ${it.size()})"
+		try
+		{
+			def tempFile = new File(it.toString());
+			resultTextBuilder <<= String.format("Processing File: %s \n", it.toString());
+			resultTextBuilder <<= String.format("File Properties: Writable: %s, Readable: %s, isDirectory: %s  \n", tempFile.canWrite(),tempFile.canRead(), tempFile.isDirectory());
+			//Is it a directory?
+			//It has to be writable and readable
+			if(!tempFile.isDirectory() && tempFile.canWrite() && tempFile.canRead())
+			{
+				if(tempFile.getText().find(filePattern) != null)
+				{
+					resultTextBuilder <<= String.format("File: %s matched the pattern %s  \n", it.name, pattern);
+					//Pattern Found - So, log the relevant data.
+
+					//Original content of the file
+					def fileContent = tempFile.getText();
+
+					//Update the file with the result of the ReplaceAll
+					tempFile.write(replaceText(fileContent));
+
+
+					//Define the backup file
+					def backupFilename = tempFile.toString().replaceFirst(~/.[^.]+$/, '');
+					def backupFile = new File(backupFilename + "-copy." + getExtensionFromFilename(tempFile.name));
+
+					resultTextBuilder <<= String.format("Creating backup file: %s  \n", backupFile.name);
+					//If it doesn´t exist
+					if(!backupFile.exists())
+					{
+						backupFile.createNewFile();
+						resultTextBuilder <<= String.format("Backup file created: %s  \n", backupFile.name);
+					}
+					//write the content of the original file
+					backupFile.write(fileContent);
+				}
+			}
+			else
+			{
+				resultTextBuilder <<= String.format("Exploring inside of folder: %s  \n", tempFile.name);
+			}	
+		}
+		catch(IOException ex)
+		{
+			resultTextBuilder <<= String.format("An error has been occurred on file: %s\n Stack Trace: \n %s", it.toString(), ex.toString());
+		}
+		catch(Exception ex)
+		{
+			resultTextBuilder <<= String.format("An unknown error has been occurred on file: %s  \nAccess Denied\n Stack Trace: \n %s", it.toString, ex.toString());
+		}
 	}
+	
+	//returns the new string with the newString instead of the matching pattern
+	def replaceText(text) { 
+		text.replaceAll(filePattern, newString);
+	} 
+	
+	//Get the extension of the file
+	def getExtensionFromFilename(filename) {
+		def returned_value = ""
+		def m = (filename =~ /(.[^.]*)$/)
+		if (m.size()>0) returned_value = ((m[0][0].size()>0) ? m[0][0].substring(1).trim().toLowerCase() : "");
+		return returned_value
+	}
+	
 }
--- src/com/st/ui/MainView.java
+++ src/com/st/ui/MainView.java
@@ -1,39 +1,39 @@
 package com.st.ui;
 
-import java.awt.Container;
-
+import com.st.src.PatternFinder;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.JTextArea;
-
-import com.st.src.PatternFinder;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
-
+import javax.swing.JTextArea;
 
 public class MainView extends JFrame  {
 	private static final long serialVersionUID = 1L;
 	private JTextField txtStringPattern;
 	private JTextField txtNewString;
 	private JTextField txtBasePath;
-	private Container cp = this.getContentPane();
+	private JTextField txtOutputFile;
+	private JTextArea txtResultArea = new JTextArea();
 	/**
 	 * Create the panel.
 	 */
 	public MainView() {
+		//JFrame properties
 		setResizable(false);
 		createUIComponents();
 		this.setTitle("Search Technologies Coding Exercise");
 		this.setSize(450, 300);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setVisible(true);
 	}
-
+	/*
+	 * Initialize GUI Components 
+	 */
 	private void createUIComponents() {
 		getContentPane().setLayout(null);
 		
@@ -78,37 +78,74 @@ public class MainView extends JFrame  {
 		btnSearchDir.setBounds(373, 102, 25, 20);
 		getContentPane().add(btnSearchDir);
 		
-		JTextArea txtResultArea = new JTextArea();
-		txtResultArea.setEditable(false);
-		txtResultArea.setBounds(10, 158, 424, 102);
-		getContentPane().add(txtResultArea);
-		
 		JButton btnSearch = new JButton("Search Pattern");
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				handleSearchAction();
 			}
 		});
-		btnSearch.setBounds(129, 125, 141, 23);
+		btnSearch.setBounds(130, 162, 141, 23);
 		getContentPane().add(btnSearch);
+		
+		txtOutputFile = new JTextField();
+		txtOutputFile.setEditable(false);
+		txtOutputFile.setColumns(10);
+		txtOutputFile.setBounds(130, 131, 233, 20);
+		getContentPane().add(txtOutputFile);
+		
+		JLabel lblOutputFile = new JLabel("Output File");
+		lblOutputFile.setBounds(10, 137, 110, 14);
+		getContentPane().add(lblOutputFile);
+		
+		JButton btnOutputFile = new JButton("...");
+		btnOutputFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				handleOutputFileSelection();
+			}
+		});
+		btnOutputFile.setBounds(373, 131, 25, 20);
+		getContentPane().add(btnOutputFile);
+		
+		
+		txtResultArea.setEditable(false);
+		txtResultArea.setBounds(10, 191, 424, 69);
+		getContentPane().add(txtResultArea);
 		
 	}
 
-	@SuppressWarnings("unused")
+	protected void handleOutputFileSelection() {
+		final JFileChooser fc = new JFileChooser(); 
+		fc.setFileSelectionMode(JFileChooser.SAVE_DIALOG);  
+		int returnVal = fc.showDialog(this,"Select Output File");  
+		if (returnVal == JFileChooser.APPROVE_OPTION)  
+		{  
+			File file = fc.getSelectedFile();
+			String sfilename = file.getPath();  
+			txtOutputFile.setText(sfilename);	
+		}		
+	}
+	/*
+	 *	Validates the params and calls the Groovy code 
+	 */
 	protected void handleSearchAction() {
-		if(txtBasePath.getText().trim().isEmpty() || txtNewString.getText().trim().isEmpty() || txtStringPattern.getText().trim().isEmpty())
+		if(txtBasePath.getText().trim().isEmpty() || txtNewString.getText().trim().isEmpty() || txtStringPattern.getText().trim().isEmpty() || txtOutputFile.getText().trim().isEmpty())
 		{
 			JOptionPane.showMessageDialog(this, "Please provide all the parameters", "Error",JOptionPane.ERROR_MESSAGE);
 		}
 		else
 		{
-			String params[] = {txtBasePath.getText().trim(), txtStringPattern.getText().trim(), txtNewString.getText().trim()};
-			PatternFinder pf = new PatternFinder();
-			pf.findInPath(params[0], params[1], params[2]);
+			String params[] = {txtBasePath.getText().trim(), txtStringPattern.getText().trim(), txtNewString.getText().trim(), txtOutputFile.getText().trim()};
+			PatternFinder pf = new PatternFinder(params);
+			long startTime =System.nanoTime();   
+			pf.findInPath();
+			double estimatedTime = (System.nanoTime() - startTime)/1000000000;   
+			txtResultArea.setText(String.format("Operation completed in %s seconds \nAn log file has been created: %s", (int)estimatedTime, params[3]));
 		}
-		
 	}
 
+	/*
+	 * Shows the JFileChooser for the Directory selection
+	 */
 	protected void handleSearchDirEvent() {
 		 final JFileChooser fc = new JFileChooser(); 
 		 fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);  
